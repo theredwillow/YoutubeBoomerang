1fb130f2c1811da86a86d3299bf5e468f6345f6b
theredwillow <theredwillows@gmail.com>
2018-02-12 22:42:03 -0600

    Got the list to update by switching it to observable collection. Started adding a converter that I want to take the boolean variable and decide whether to display the play or pause bitmap static resource. Unfortunately, my preliminary margin test for the converter isn't even working, but it's bedtime!

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -16,6 +16,7 @@ using Windows.UI.ViewManagement;
 using Windows.UI.Popups;
 using Windows.UI.Core;
 using Windows.System;
+using System.Collections.ObjectModel;
 
 namespace App1
 {
@@ -40,15 +41,11 @@ namespace App1
             public string Media = MainPage.Media;
             public int Quality = MainPage.Quality;
             public int Percentage = 0;
-
-            public void StartDownloading()
-            {
-                DownloadingTitles.Add(this);
-            }
+            public bool IsPaused = true;
         }
 
         // List to keep track of all that's been downloaded
-        static public List<TitleToDownload> DownloadingTitles = new List<TitleToDownload>();
+        static public ObservableCollection<TitleToDownload> DownloadingTitles = new ObservableCollection<TitleToDownload>();
 
         // Initializing Function
         public MainPage()
@@ -64,8 +61,8 @@ namespace App1
         {
             MainPage.TitleToDownload NewDownload = new MainPage.TitleToDownload();
             NewDownload.Title = NewTitle;
-            NewDownload.StartDownloading();
-            NowDownloadingList.ItemsSource = DownloadingTitles;
+            DownloadingTitles.Add(NewDownload);
+            NowDownloadingCollection.ItemsSource = DownloadingTitles;
         }
 
         /// <summary>
@@ -73,6 +70,7 @@ namespace App1
         /// Used to select whether to download whole videos or just their audio.
         /// </summary>
         /// <param name="sender">The media radio button that was pressed.</param>
+        /// <param name="e"></param>
         private void MediaRadioButton_Changed(object sender, RoutedEventArgs e)
         {
             Media = Convert.ToString( ( (RadioButton)sender ).Tag );
@@ -87,6 +85,7 @@ namespace App1
         /// For example, in the case that only two are available, Low or High, less than 50% would be Low and greater than or equal to 50% would be high.
         /// </remarks>
         /// <param name="sender">The quality slider.</param>
+        /// <param name="e"></param>
         private void Slider_QualityChanged(object sender, RoutedEventArgs e)
         {
             Quality = Convert.ToInt16( ( (Slider)sender ).Value );
@@ -97,6 +96,7 @@ namespace App1
         /// Used to select whether to look for videos or whole playlists.
         /// </summary>
         /// <param name="sender">The quantity radio button that was pressed.</param>
+        /// <param name="e"></param>
         private void QuantityRadioButton_Changed(object sender, RoutedEventArgs e)
         {
             Quantity = Convert.ToString( ( (RadioButton)sender ).Tag );
@@ -107,6 +107,7 @@ namespace App1
         /// Used to select whether the download starts instantly or waits for the user to press play.
         /// </summary>
         /// <param name="sender">The wait for checkbox.</param>
+        /// <param name="e"></param>
         private void WaitForCheckbox_Changed(object sender, RoutedEventArgs e)
         {
             WaitForSelection = Convert.ToBoolean(((CheckBox)sender).IsChecked);
@@ -117,6 +118,7 @@ namespace App1
         /// Used to start or queue a download, depending on the wait for variable.
         /// </summary>
         /// <param name="sender">The title textbox.</param>
+        /// <param name="e"></param>
         private void TextBox_KeyDown(object sender, KeyRoutedEventArgs e)
         {
             if(e.Key == VirtualKey.Enter)
@@ -125,6 +127,33 @@ namespace App1
                 PrepareDownload(Convert.ToString(titleBox.Text));
                 titleBox.Text = "";
             }
+        }
+    }
+
+}
+
+namespace Converters
+{
+    public class BooleanBitmapConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool IsPaused = (bool)value;
+            if (IsPaused == true)
+            {
+                return "5";
+            }
+            else
+            {
+                return "10";
+            }
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
         }
     }
 }
